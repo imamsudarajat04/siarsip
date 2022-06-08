a6b67c8524fd3b8612549533589fb6d6f4d15d4c
imamsudarajat04 <imamsudrajat707@gmail.com>
2022-06-08 10:22:11 +0700

    Fix Response Login dan Response Logout, Token Fix Berhasil

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//Api Login
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//Api Logout
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
