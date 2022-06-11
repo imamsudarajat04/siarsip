ad2e5bb4d9f2a9ced58b2ad06d1d53a6adf54ef2
imamsudarajat04 <imamsudrajat707@gmail.com>
2022-06-11 11:09:57 +0700

    Fix Mengatasi Error saat login yang gak mau pindah ke halaman dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Login from "./components/Login/Login";
-import { Outlet, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard"
 
 const App = () => {
--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,6 +1,48 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate  } from 'react-router';
 
 function Login() {
+
+    //define state
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    //define state validation
+    const [validation, setValidation] = useState([]);
+
+    //define navigate
+    const navigate = useNavigate();
+
+    //function "loginHanlder"
+    const loginHandler = async (e) => {
+        e.preventDefault();
+        
+        //initialize formData
+        const formData = new FormData();
+
+        //append data to formData
+        formData.append('email', email);
+        formData.append('password', password);
+
+        //send data to server
+        await axios.post('http://127.0.0.1:8000/api/login', formData)
+        .then((response) => {
+
+            console.log(response);
+
+            //set token on localStorage
+            localStorage.setItem('token', response.data.token);
+
+            //redirect to dashboard
+            navigate('/dashboard');
+        })
+        .catch((error) => {
+
+            //assign error to state "validation"
+            setValidation(error.response.data);
+        })
+    };
 
     return (
         <div className="container" style={{ marginTop: "120px" }}>
@@ -8,7 +50,42 @@ function Login() {
                 <div className="col-md-4">
                     <div className="card border-0 rounded shadow-sm">
                         <div className="card-body">
-                            HALAMAN LOGIN
+                            <h4 className="fw-bold">HALAMAN LOGIN</h4>
+                            <hr/>
+                            {
+                                validation.message && (
+                                    <div className="alert alert-danger">
+                                        {validation.message}
+                                    </div>
+                                )
+                            }
+                            <form onSubmit={loginHandler}>
+                                <div className="mb-3">
+                                    <label className="form-label">ALAMAT EMAIL</label>
+                                    <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Masukkan Alamat Email"/>
+                                </div>
+                                {
+                                    validation.email && (
+                                        <div className="alert alert-danger">
+                                            {validation.email[0]}
+                                        </div>
+                                    )
+                                }
+                                <div className="mb-3">
+                                    <label className="form-label">PASSWORD</label>
+                                    <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Masukkan Password"/>
+                                </div>
+                                {
+                                    validation.password && (
+                                        <div className="alert alert-danger">
+                                            {validation.password[0]}
+                                        </div>
+                                    )
+                                }
+                                <div className="d-grid gap-2">
+                                    <button type="submit" className="btn btn-primary">LOGIN</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
